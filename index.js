e1df5955de8ccc0ef0a50549d67795c4757cc8f9
IMNAP <leto99999@gmail.com>
2022-08-30 12:49:21 +0300

    chore: rid off draft

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-
-import express from "express";
-import cors from 'cors';
-import path  from "path";
-import fs from 'fs';
-
+// import express from "express";
+// import cors from 'cors';
+// import path  from "path";
+// import fs from 'fs';
+// import axios from 'axios'
+// import  leads from './answer'
 // console.log(Client);
 
-// const express = require("express") ;
-// const cors = require('cors') ;
-// const path  = require("path") ;
-// const cl = require('amocrm-js')
+const express = require("express");
+const cors = require('cors');
+const path = require("path");
+const axios = require('axios');
+require('dotenv').config()
+
+console.log(process.env.TOKEN);
+// const leads = require('./answer');
 
 const app = express();
 
 const PORT = process.env.SERVER_PORT || 3000;
+
 app.use(cors({
     origin: '*'
 }));
@@ -21,103 +26,42 @@ app.use(cors({
 app.use(express.static(path.resolve() + "/public"));
 app.use(express.json());
 
-let events = [{
-        date: '2022-05-06',
-        name: 'event1'
-    },
-    {
-        date: '2022-05-06',
-        name: 'event2'
-    },
-    {
-        date: '2022-05-07',
-        name: 'event3'
-    },
-    {
-        date: '2022-05-07',
-        name: 'event4'
-    },
-    {
-        date: '2022-05-08',
-        name: 'event5'
-    },
-]
 
-const amoJson = {"_page":1,"_links":{"self":{"href":"https://alekseirizchkov.amocrm.ru/api/v4/leads?page=1&limit=50"}},"_embedded":{"leads":[{"id":2841691,"name":"Запрос тарифов","price":14000,"responsible_user_id":8499319,"group_id":0,"status_id":50272636,"pipeline_id":5721550,"loss_reason_id":null,"created_by":8499319,"updated_by":8499319,"created_at":1661709389,"updated_at":1661760858,"closed_at":null,"closest_task_at":null,"is_deleted":false,"custom_fields_values":null,"score":null,"account_id":30363148,"labor_cost":null,"_links":{"self":{"href":"https://alekseirizchkov.amocrm.ru/api/v4/leads/2841691?page=1&limit=50"}},"_embedded":{"tags":[],"companies":[]}},{"id":2841687,"name":"Рекламная продукция","price":11000,"responsible_user_id":8499319,"group_id":0,"status_id":50272636,"pipeline_id":5721550,"loss_reason_id":null,"created_by":8499319,"updated_by":8499319,"created_at":1661709383,"updated_at":1661761131,"closed_at":null,"closest_task_at":null,"is_deleted":false,"custom_fields_values":null,"score":null,"account_id":30363148,"labor_cost":null,"_links":{"self":{"href":"https://alekseirizchkov.amocrm.ru/api/v4/leads/2841687?page=1&limit=50"}},"_embedded":{"tags":[],"companies":[]}}]}}
-
-// console.log(JSON.stringify(amoJson))
-// console.log(JSON.parse(amoJson))
 
 app.get('/', (req, res) => {
-    // res.send('ok')
     res.render('index.html')
 });
 
-import {Client}  from "amocrm-js";
-
-const crm = new Client({
-    domain: 'alekseirizchkov',
-    auth: {
-        client_id: 'ac7028ff-5671-4bdf-9254-c4cc32d9305f',
-        client_secret: 'ORte0zkC3D6THghrUVutvyuo0Rla2FK5TJ2uL4mku8VdiB6UvHX6KwY9zRxSEj4c',
-        redirect_uri: 'https://newartspace.ru',
-        code: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImQ0ZDUxYmNjZDc0YjY3Y2MyODkyMGY5NDkzZmRlN2U1ZTRhZjc0ZjkwODgxMDM3YTM0NDlhN2RjOWExMjY0ZjY3OWQ5MmQ1OWEwZThlYmJjIn0.eyJhdWQiOiI1YjY5NmFiYy0wYTI3LTQ5ODMtOTg5ZS1jM2FiYzZlZTBlMjQiLCJqdGkiOiJkNGQ1MWJjY2Q3NGI2N2NjMjg5MjBmOTQ5M2ZkZTdlNWU0YWY3NGY5MDg4MTAzN2EzNDQ5YTdkYzlhMTI2NGY2NzlkOTJkNTlhMGU4ZWJiYyIsImlhdCI6MTY2MTcxMDA0MSwibmJmIjoxNjYxNzEwMDQxLCJleHAiOjE2NjE3OTY0NDEsInN1YiI6Ijg0OTkzMTkiLCJhY2NvdW50X2lkIjozMDM2MzE0OCwic2NvcGVzIjpbInB1c2hfbm90aWZpY2F0aW9ucyIsImZpbGVzIiwiY3JtIiwiZmlsZXNfZGVsZXRlIiwibm90aWZpY2F0aW9ucyJdfQ.HSTTM_AIRVVSeeRK-Br7cfxpeJke-OZpVvZXl4Py5yn0l0-nweoq_cq9xvU4tf24rsIPE6QYb-XCWDBCRBzGbTjIh4HnxSfTvoximh6cOpqFg2ud_8qwLi11PIisiIJ3Kyxgj32ei2ndek6aOLQWuG7PMql8kgazvpqqB1_vnzxRpzLblKkO1O2uw2RVfufnlCnNTOOJhslfcczfdy2eu1hMoIO4MaAIOudeyNDMj2hHxPZM_NwPn_nKdpELw4LBIOJCL5fM1s1SG1hHFJPRZTMkZr4T1PYw_lzO3tjXB00cASsuXDqIN92z8F5E-iiPxmfXNWJ9z6XwSat-ZOw31w'
-    },
-} )
-
-// const a = async () => {
-//     const data = await crm.request.get('/api/v4/leads');
-// }
-// a()
-
-// console.log(a)
-
-// fs.writeFile(path.resolve(path.resolve(), 'error.txt'), a(), (err) => {
-//     if (err) {
-//         console.log(errorMessage(err.message));
-//     } else {
-//         console.log(successMessage('файл сохранен'));
-//     }
-// });
-
-// a()
-// console.log(a())
+const token = 
 
 
 
 
-app.get('/events/all', (req, res) => {
+app.get('/api/leads', (req, res) => {
 
-    res.setHeader('Content-Type', 'text/json');
-    res.setHeader('Content-Type', 'application/json');
+    async function getAmoLeads() {
+        let answer = await axios({
+            method: 'get',
+            url: 'https://alekseirizchkov.amocrm.ru/api/v4/leads',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': "application/json"
+            },
+        })
 
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000/events/all/");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-    res.json(events);
-});
-// crm()
-app.get('/events/:date', (req, res) => {
+        const responseBody = answer.data
+        console.log(responseBody)
+        res.send(responseBody);
 
-    const array = events.filter((event) => {
-        if (req.params.date === event.date) {
-            return true;
-        } else {
-            return false;
-        }
-    });
+    }
 
-    let answer = JSON.stringify(array);
-    res.setHeader(
-        'Content-Type', 'text/json'
+    getAmoLeads()
 
 
-    );
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.send(answer);
 });
 
-// const server = http.createServer(app)
+
+
 
 app.listen(PORT, (req, res) => {
     console.log(`Server is working on port ${PORT}`);
--- public/script.js
+++ public/script.js
@@ -1,49 +1,19 @@
-const btn = document.querySelector("button");
 
-// async function query() {
+getLeads()
 
-//     // const query = await fetch("http://localhost:3000/events/all", {
-//     const query = await fetch("https://alekseirizchkov.amocrm.ru/api/v4/leads", {
-//         mode: "no-cors",
-//         method: "get",
-//         headers: {
-//             "Content-Type": "application/json",
-//             "Access-Control-Allow-Origin": "*",
-//             "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImQ0ZDUxYmNjZDc0YjY3Y2MyODkyMGY5NDkzZmRlN2U1ZTRhZjc0ZjkwODgxMDM3YTM0NDlhN2RjOWExMjY0ZjY3OWQ5MmQ1OWEwZThlYmJjIn0.eyJhdWQiOiI1YjY5NmFiYy0wYTI3LTQ5ODMtOTg5ZS1jM2FiYzZlZTBlMjQiLCJqdGkiOiJkNGQ1MWJjY2Q3NGI2N2NjMjg5MjBmOTQ5M2ZkZTdlNWU0YWY3NGY5MDg4MTAzN2EzNDQ5YTdkYzlhMTI2NGY2NzlkOTJkNTlhMGU4ZWJiYyIsImlhdCI6MTY2MTcxMDA0MSwibmJmIjoxNjYxNzEwMDQxLCJleHAiOjE2NjE3OTY0NDEsInN1YiI6Ijg0OTkzMTkiLCJhY2NvdW50X2lkIjozMDM2MzE0OCwic2NvcGVzIjpbInB1c2hfbm90aWZpY2F0aW9ucyIsImZpbGVzIiwiY3JtIiwiZmlsZXNfZGVsZXRlIiwibm90aWZpY2F0aW9ucyJdfQ.HSTTM_AIRVVSeeRK-Br7cfxpeJke-OZpVvZXl4Py5yn0l0-nweoq_cq9xvU4tf24rsIPE6QYb-XCWDBCRBzGbTjIh4HnxSfTvoximh6cOpqFg2ud_8qwLi11PIisiIJ3Kyxgj32ei2ndek6aOLQWuG7PMql8kgazvpqqB1_vnzxRpzLblKkO1O2uw2RVfufnlCnNTOOJhslfcczfdy2eu1hMoIO4MaAIOudeyNDMj2hHxPZM_NwPn_nKdpELw4LBIOJCL5fM1s1SG1hHFJPRZTMkZr4T1PYw_lzO3tjXB00cASsuXDqIN92z8F5E-iiPxmfXNWJ9z6XwSat-ZOw31w",
-        
-//         }
-//     })
-//     console.log(query.text())
-//     const response = await query.text()
-//     console.log(response)
-//     console.log(111) 
-//     console.log(JSON.stringify(query))
-// }
-// query()
-
-var myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
-myHeaders.append("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImQ0ZDUxYmNjZDc0YjY3Y2MyODkyMGY5NDkzZmRlN2U1ZTRhZjc0ZjkwODgxMDM3YTM0NDlhN2RjOWExMjY0ZjY3OWQ5MmQ1OWEwZThlYmJjIn0.eyJhdWQiOiI1YjY5NmFiYy0wYTI3LTQ5ODMtOTg5ZS1jM2FiYzZlZTBlMjQiLCJqdGkiOiJkNGQ1MWJjY2Q3NGI2N2NjMjg5MjBmOTQ5M2ZkZTdlNWU0YWY3NGY5MDg4MTAzN2EzNDQ5YTdkYzlhMTI2NGY2NzlkOTJkNTlhMGU4ZWJiYyIsImlhdCI6MTY2MTcxMDA0MSwibmJmIjoxNjYxNzEwMDQxLCJleHAiOjE2NjE3OTY0NDEsInN1YiI6Ijg0OTkzMTkiLCJhY2NvdW50X2lkIjozMDM2MzE0OCwic2NvcGVzIjpbInB1c2hfbm90aWZpY2F0aW9ucyIsImZpbGVzIiwiY3JtIiwiZmlsZXNfZGVsZXRlIiwibm90aWZpY2F0aW9ucyJdfQ.HSTTM_AIRVVSeeRK-Br7cfxpeJke-OZpVvZXl4Py5yn0l0-nweoq_cq9xvU4tf24rsIPE6QYb-XCWDBCRBzGbTjIh4HnxSfTvoximh6cOpqFg2ud_8qwLi11PIisiIJ3Kyxgj32ei2ndek6aOLQWuG7PMql8kgazvpqqB1_vnzxRpzLblKkO1O2uw2RVfufnlCnNTOOJhslfcczfdy2eu1hMoIO4MaAIOudeyNDMj2hHxPZM_NwPn_nKdpELw4LBIOJCL5fM1s1SG1hHFJPRZTMkZr4T1PYw_lzO3tjXB00cASsuXDqIN92z8F5E-iiPxmfXNWJ9z6XwSat-ZOw31w");
-myHeaders.append("Cookie", "session_id=j56m9g5hhh9lrolqg0unvohm8j; user_lang=ru; csrf_token=def50200d5b8d0948f0e4652b58455d38180db224d58ef6e7e599f15543411f483cc775e7bc1911204f68db961c9e682ffdf2bc34812c21ae6e0ff66c4294316db8516e09c4dcba339c6fe9292bd6985868c9d7995cfebad31521aaeb7ec8f7968e5121a6bbf073b5912547d62ef8c35bf05cc7efa024c64fda9fa01c9c40d3924e706c455bea4059ab2b17efac9d6ee86f3fc4dc1033bc0ca2045f9ce1a248653428821a9582964d650a9d2053b5bb42c5e76a3fc4e65dea6bae604b049a76fc084c04322efb48ae63baa93e37bcce642e3b8f0a547208b3690453c2f6632235b18e5ccad9a0ea4edd0c140a635bf7be10b646401544fc048b502c172bfe1807c59986c02b74f78988d02");
-
-var raw = "";
-
-var requestOptions = {
-  method: 'GET',
-  headers: myHeaders,
-//   body: raw,
-  redirect: 'follow'
-};
-
-fetch("https://alekseirizchkov.amocrm.ru/api/v4/leads", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
-
-//     fetch("https://jsonplaceholder.typicode.com/todos/1")
-//   .then(response => response.json())
-//   .then(json => console.log(json))
+async function getLeads() {
+    const query = await fetch("http://localhost:3000/api/leads", {
+        mode: "no-cors",
+        method: "get",
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImQ0ZDUxYmNjZDc0YjY3Y2MyODkyMGY5NDkzZmRlN2U1ZTRhZjc0ZjkwODgxMDM3YTM0NDlhN2RjOWExMjY0ZjY3OWQ5MmQ1OWEwZThlYmJjIn0.eyJhdWQiOiI1YjY5NmFiYy0wYTI3LTQ5ODMtOTg5ZS1jM2FiYzZlZTBlMjQiLCJqdGkiOiJkNGQ1MWJjY2Q3NGI2N2NjMjg5MjBmOTQ5M2ZkZTdlNWU0YWY3NGY5MDg4MTAzN2EzNDQ5YTdkYzlhMTI2NGY2NzlkOTJkNTlhMGU4ZWJiYyIsImlhdCI6MTY2MTcxMDA0MSwibmJmIjoxNjYxNzEwMDQxLCJleHAiOjE2NjE3OTY0NDEsInN1YiI6Ijg0OTkzMTkiLCJhY2NvdW50X2lkIjozMDM2MzE0OCwic2NvcGVzIjpbInB1c2hfbm90aWZpY2F0aW9ucyIsImZpbGVzIiwiY3JtIiwiZmlsZXNfZGVsZXRlIiwibm90aWZpY2F0aW9ucyJdfQ.HSTTM_AIRVVSeeRK-Br7cfxpeJke-OZpVvZXl4Py5yn0l0-nweoq_cq9xvU4tf24rsIPE6QYb-XCWDBCRBzGbTjIh4HnxSfTvoximh6cOpqFg2ud_8qwLi11PIisiIJ3Kyxgj32ei2ndek6aOLQWuG7PMql8kgazvpqqB1_vnzxRpzLblKkO1O2uw2RVfufnlCnNTOOJhslfcczfdy2eu1hMoIO4MaAIOudeyNDMj2hHxPZM_NwPn_nKdpELw4LBIOJCL5fM1s1SG1hHFJPRZTMkZr4T1PYw_lzO3tjXB00cASsuXDqIN92z8F5E-iiPxmfXNWJ9z6XwSat-ZOw31w",
+        }
+    })
+    const answer = await query.text()
+    console.log(typeof answer)
+}
 
 
 
-// btn.addEventListener("click", query)
